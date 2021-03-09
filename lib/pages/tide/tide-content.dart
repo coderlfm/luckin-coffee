09e7a4d200bb90146407b9c068da0123efdb00b2
宿愿Cc <375642570@qq.com>
2021-03-09 21:43:15 +0800

    feat(tide): 完成潮品活动图片渲染

--- lib/pages/tide/tide-content.dart
+++ lib/pages/tide/tide-content.dart
@@ -14,10 +14,6 @@ class TideContent extends StatelessWidget {
       child: Selector<TideViewModel, TideViewModel>(
         selector: (ctx, tiVM) => tiVM,
         shouldRebuild: (prev, next) {
-          // print('${prev.tideBannerData.length}  ${prev.tideData.length}  ${next.tideBannerData.length}  ${next.tideData.length}');
-
-          /// TODO: 此处判断有点问题
-          // return (prev.tideBannerData.length != next.tideBannerData.length || prev.tideData.length != next.tideData.length);
           return next.tideBannerData.length != 0 || next.tideData.length != 0;
         },
         builder: (ctx, tiVM, child) {
@@ -40,7 +36,7 @@ class TideContent extends StatelessWidget {
                       shrinkWrap: true,
                       physics: ScrollPhysics(),
                       itemBuilder: (ctx, index) {
-                        return checkBuildType(tidata[index + 1]);
+                        return checkBuildType(context, tidata[index + 1]);
                       }),
                 )
               ],
@@ -54,7 +50,7 @@ class TideContent extends StatelessWidget {
   }
 
   /// 校验 构建的 widget 类型
-  Widget checkBuildType(TideModel tidata) {
+  Widget checkBuildType(BuildContext context, TideModel tidata) {
     late Widget widget;
 
     switch (tidata.componentType) {
@@ -70,11 +66,139 @@ class TideContent extends StatelessWidget {
         widget = buildChannel(tidata);
         break;
 
+      case 14:
+        widget = buildFlashSale(context, tidata);
+        break;
+
+      case 4:
+        widget = buildActivityImg(tidata);
+        break;
+
       default:
         widget = Text('商品组件');
     }
 
     return widget;
+  }
+
+  /// 构建 活动图片
+  Widget buildActivityImg(TideModel tidata) {
+    return GridView.builder(
+      padding: EdgeInsets.zero,
+      physics: ScrollPhysics(),
+      shrinkWrap: true,
+      itemCount: tidata.subComponents!.length,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 10.px,
+        mainAxisExtent: 95.px,
+      ),
+      itemBuilder: (ctx, index) {
+        return Image.network(
+          tidata.subComponents![index].backGroundUrl,
+          height: double.minPositive,
+        );
+      },
+    );
+  }
+
+  /// 构建 限时秒杀
+  Widget buildFlashSale(BuildContext context, TideModel tidata) {
+    /// 当前只构建一条秒杀
+    final flashSale = tidata.flashSaleComponentDetailS![0];
+    final nowTime = DateTime.now().microsecondsSinceEpoch;
+
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 15.px, horizontal: 10.px),
+      margin: EdgeInsets.symmetric(vertical: 8.px),
+      decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(10.px)),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Text('限时秒杀', style: Theme.of(context).textTheme.headline2!.copyWith(fontWeight: FontWeight.bold)),
+              SizedBox(width: 10.px),
+              Container(
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  // color: Colors.red,
+                  borderRadius: BorderRadius.circular(100.px),
+                  border: Border.all(color: Colors.red, width: 1.px),
+                ),
+                child: Row(
+                  children: [
+                    Container(
+                        padding: EdgeInsets.symmetric(horizontal: 5.px),
+                        decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.circular(100.px),
+                          // border: Border.all(color: Colors.red, width: 1.px),
+                        ),
+                        child: Text('距结束', style: Theme.of(context).textTheme.headline1!.copyWith(color: Colors.white))),
+                    Text('${DateTime.fromMicrosecondsSinceEpoch((flashSale.endTimeCountdown * 1000 * 1000) - nowTime)}', style: Theme.of(context).textTheme.bodyText1!.copyWith(color: Colors.black)),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          buildFlashSaleList(flashSale, context)
+        ],
+      ),
+    );
+  }
+
+  /// 构建限时秒杀商品列表
+  Container buildFlashSaleList(FlashSaleComponentDetail flashSale, BuildContext context) {
+    return Container(
+      height: 120.px,
+      child: ListView.builder(
+        padding: EdgeInsets.zero,
+        physics: ScrollPhysics(),
+        scrollDirection: Axis.horizontal,
+        shrinkWrap: true,
+        itemCount: flashSale.commodityList.length,
+        itemBuilder: (ctx, index) {
+          final fsItem = flashSale.commodityList[index];
+          return Container(
+            width: 100.px,
+            padding: EdgeInsets.only(top: 10.px, right: 10.px),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ClipRRect(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(5.px),
+                      topRight: Radius.circular(5.px),
+                    ),
+                    child: Image.network(fsItem.defaultPicUrl)),
+                SizedBox(height: 5.px),
+                Text(
+                  fsItem.name,
+                  softWrap: false,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: Theme.of(context).textTheme.bodyText1!.copyWith(fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 5.px),
+                Row(
+                  children: [
+                    Text('￥${fsItem.skillPrice}', style: Theme.of(context).textTheme.bodyText1!.copyWith(fontWeight: FontWeight.bold, color: Color(0xffC11C2B))),
+                    Text(
+                      '￥${fsItem.price}',
+                      style: Theme.of(context).textTheme.headline1!.copyWith(
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xff999999),
+                            decoration: TextDecoration.lineThrough,
+                          ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
   }
 
   /// 构建轮播底部图片
